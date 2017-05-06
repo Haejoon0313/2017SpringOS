b61a3a0c861410e514fc5547e755ef4a4f9d26a6
Haejoon0313 <sam0313@kaist.ac.kr>
2017-05-06 23:51:14 +0900

    code finished but no debugging

--- pintos/src/vm/page.h
+++ pintos/src/vm/page.h
@@ -1,4 +1,5 @@
 #include <list.h>
+#include <hash.h>
 #include "threads/palloc.h"
 #include "threads/thread.h"
 
@@ -20,11 +21,6 @@ struct sup_pte
 void page_table_init(struct hash * page_table);
 void page_table_remove(struct hash * page_table);
 
-bool page_insert(struct file * file, off_t ofs, uint8_t * upage, uint32_t read_bytes, uint32_t zero_bytes, bool writable);
+bool page_insert(struct file * file, off_t file_ofs, uint8_t * upage, uint32_t read_bytes, uint32_t zero_bytes, bool writable);
 struct sup_pte * get_sup_pte(struct hash * page_table, void * upage);
 bool page_remove(struct hash * page_table, void * upage);
-
-void * grow_stack(void * upage);
-
-void page_table_lock_acquire(void);
-void page_table_lock_release(void);
